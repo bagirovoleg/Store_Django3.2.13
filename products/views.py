9af22dd41da8a6517b695f8695fbbce07b94200c
Oleg Bagirov <88608903+bagirovoleg@users.noreply.github.com>
2023-03-05 21:19:42 +0100

    ver.3.0: -Dry -Media files

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from products.models import Product, ProductCategory
+
+def index(request):
+    return render(request, 'products/index.html')
+
+def products(request):
+    context = {
+        'title': 'Products',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
